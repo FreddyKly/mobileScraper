e882c1ee548ac1dd1d955f1de029fa9149f9f6b5
Freddy <Frederik.Kliemt@web.de>
2020-04-30 13:29:08 +0200

    Added BeatifulSoup

--- Scraper/mobileScraper.py
+++ Scraper/mobileScraper.py
@@ -1,7 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 class mobileScraper():
     def __init__(self, url):
         self.url = url
         self.session = requests.Session()
+        self.source = self.session.get(url).text
+        self.soup = BeautifulSoup(self.source, "html.parser")
+        print(self.soup.prettify())
